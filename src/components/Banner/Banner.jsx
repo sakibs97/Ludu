4e8091d10d2e9f96ec0b33fe7a1ca7c57a6ec245
Sakib Hossain <sakibs.ngn@gmail.com>
2024-12-22 01:19:46 +0600

    3rd commit

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,4 +1,6 @@
 import './banner.css'
+import arrow from '../../assets/arrow.png'
+import { IoMdStar } from "react-icons/io";
 
 const Banner = () => {
     return (
@@ -10,32 +12,39 @@ const Banner = () => {
                         <div className="part">
                             <div className="red_box">
                                 <div className="red_box_middle">
-                                    <div className="red_logo"></div>
-                                    <div className="red_logo"></div>
-                                    <div className="red_logo"></div>
-                                    <div className="red_logo"></div>
+                                    <div className="red_logo shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"></div>
+                                    <div className="red_logo shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"></div>
+                                    <div className="red_logo shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"></div>
+                                    <div className="red_logo shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"></div>
                                 </div>
                             </div>
                             <div className="part_one_middle">
-                                <div className="row">
+                                <div className="row relative">
+                                    <div className="col_r1"></div>
+                                    <div className="col_r1"></div>
+                                    <div className="col_r1">
+                                        <IoMdStar className='w-8 h-8 mx-auto my-[6px] text-[#15792a]' />
+                                    </div>
                                     <div className="col_r1"></div>
                                     <div className="col_r1"></div>
                                     <div className="col_r1"></div>
-                                    <div className="col_r1"></div>
-                                    <div className="col_r1"></div>
-                                    <div className="col_r1"></div>
+                                    <div className="absolute top-[20px] left-[20px]">
+                                        <img src={arrow} alt="" className='w-[200px] h-[100px] scale-125' />
+                                    </div>
                                 </div>
                                 <div className="row">
                                     <div className="col_r1"></div>
-                                    <div className="col_r1"></div>
-                                    <div className="col_r1"></div>
-                                    <div className="col_r1"></div>
-                                    <div className="col_r1"></div>
-                                    <div className="col_r1"></div>
+                                    <div className="col_r1 col_green"></div>
+                                    <div className="col_r1 col_green"></div>
+                                    <div className="col_r1 col_green"></div>
+                                    <div className="col_r1 col_green"></div>
+                                    <div className="col_r1 col_green"></div>
                                 </div>
                                 <div className="row">
                                     <div className="col_r1"></div>
-                                    <div className="col_r1"></div>
+                                    <div className="col_r1 col_green">
+                                        <IoMdStar className='w-8 h-8 mx-auto my-[6px] text-yellow-50' />
+                                    </div>
                                     <div className="col_r1"></div>
                                     <div className="col_r1"></div>
                                     <div className="col_r1"></div>
@@ -44,10 +53,10 @@ const Banner = () => {
                             </div>
                             <div className="green_box">
                                 <div className="green_box_middle">
-                                    <div className="green_logo"></div>
-                                    <div className="green_logo"></div>
-                                    <div className="green_logo"></div>
-                                    <div className="green_logo"></div>
+                                    <div className="green_logo shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"></div>
+                                    <div className="green_logo shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"></div>
+                                    <div className="green_logo shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"></div>
+                                    <div className="green_logo shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"></div>
                                 </div>
                             </div>
                         </div>
@@ -57,15 +66,9 @@ const Banner = () => {
                             <div className="middle_part">
                                 <div className="mid_row">
                                     <div className="middle_row"></div>
-                                    <div className="middle_row"></div>
-                                    <div className="middle_row"></div>
-                                    <div className="middle_row"></div>
-                                    <div className="middle_row"></div>
-                                    <div className="middle_row"></div>
-                                </div>
-                                <div className="mid_row">
-                                    <div className="middle_row"></div>
-                                    <div className="middle_row"></div>
+                                    <div className="middle_row col_red">
+                                        <IoMdStar className='w-8 h-8 mx-auto my-[6px] text-yellow-50' />
+                                    </div>
                                     <div className="middle_row"></div>
                                     <div className="middle_row"></div>
                                     <div className="middle_row"></div>
@@ -73,19 +76,25 @@ const Banner = () => {
                                 </div>
                                 <div className="mid_row">
                                     <div className="middle_row"></div>
+                                    <div className="middle_row col_red"></div>
+                                    <div className="middle_row col_red"></div>
+                                    <div className="middle_row col_red"></div>
+                                    <div className="middle_row col_red"></div>
+                                    <div className="middle_row col_red"></div>
+                                </div>
+                                <div className="mid_row">
                                     <div className="middle_row"></div>
                                     <div className="middle_row"></div>
+                                    <div className="middle_row">
+                                        <IoMdStar className='w-8 h-8 mx-auto my-[6px] text-[#f12323]' />
+                                    </div>
                                     <div className="middle_row"></div>
                                     <div className="middle_row"></div>
                                     <div className="middle_row"></div>
                                 </div>
                             </div>
-                            <div className="center_part">
-                                <div className="color">
-                                    <div className="green_g"></div>
-                                    <div className="yellow_y"></div>
-                                    <div className="blue_b"></div>
-                                    <div className="red_r"></div>
+                            <div className="center_part shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px]">
+                                <div className="color ">
                                 </div>
                             </div>
                             <div className="middle_part">
@@ -93,8 +102,18 @@ const Banner = () => {
                                     <div className="middle_row"></div>
                                     <div className="middle_row"></div>
                                     <div className="middle_row"></div>
+                                    <div className="middle_row">
+                                        <IoMdStar className='w-8 h-8 mx-auto my-[6px] text-[#f8fc31]' />
+                                    </div>
                                     <div className="middle_row"></div>
                                     <div className="middle_row"></div>
+                                </div>
+                                <div className="mid_row">
+                                    <div className="middle_row col_yellow"></div>
+                                    <div className="middle_row col_yellow"></div>
+                                    <div className="middle_row col_yellow"></div>
+                                    <div className="middle_row col_yellow"></div>
+                                    <div className="middle_row col_yellow"></div>
                                     <div className="middle_row"></div>
                                 </div>
                                 <div className="mid_row">
@@ -102,15 +121,9 @@ const Banner = () => {
                                     <div className="middle_row"></div>
                                     <div className="middle_row"></div>
                                     <div className="middle_row"></div>
-                                    <div className="middle_row"></div>
-                                    <div className="middle_row"></div>
-                                </div>
-                                <div className="mid_row">
-                                    <div className="middle_row"></div>
-                                    <div className="middle_row"></div>
-                                    <div className="middle_row"></div>
-                                    <div className="middle_row"></div>
-                                    <div className="middle_row"></div>
+                                    <div className="middle_row col_yellow">
+                                        <IoMdStar className='w-8 h-8 mx-auto my-[6px] text-yellow-50' />
+                                    </div>
                                     <div className="middle_row"></div>
                                 </div>
                             </div>
@@ -120,10 +133,10 @@ const Banner = () => {
                         <div className="part">
                             <div className="blue_box">
                                 <div className="blue_box_middle">
-                                    <div className="blue_logo"></div>
-                                    <div className="blue_logo"></div>
-                                    <div className="blue_logo"></div>
-                                    <div className="blue_logo"></div>
+                                    <div className="blue_logo shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"></div>
+                                    <div className="blue_logo shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"></div>
+                                    <div className="blue_logo shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"></div>
+                                    <div className="blue_logo shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"></div>
                                 </div>
                             </div>
                             <div className="part_one_middle">
@@ -132,32 +145,36 @@ const Banner = () => {
                                     <div className="col_r1"></div>
                                     <div className="col_r1"></div>
                                     <div className="col_r1"></div>
+                                    <div className="col_r1 col_blue">
+                                        <IoMdStar className='w-8 h-8 mx-auto my-[6px] text-yellow-50' />
+                                    </div>
                                     <div className="col_r1"></div>
+                                </div>
+                                <div className="row">
+                                    <div className="col_r1 col_blue"></div>
+                                    <div className="col_r1 col_blue"></div>
+                                    <div className="col_r1 col_blue"></div>
+                                    <div className="col_r1 col_blue"></div>
+                                    <div className="col_r1 col_blue"></div>
                                     <div className="col_r1"></div>
                                 </div>
                                 <div className="row">
                                     <div className="col_r1"></div>
                                     <div className="col_r1"></div>
                                     <div className="col_r1"></div>
-                                    <div className="col_r1"></div>
-                                    <div className="col_r1"></div>
-                                    <div className="col_r1"></div>
-                                </div>
-                                <div className="row">
-                                    <div className="col_r1"></div>
-                                    <div className="col_r1"></div>
-                                    <div className="col_r1"></div>
-                                    <div className="col_r1"></div>
+                                    <div className="col_r1">
+                                        <IoMdStar className='w-8 h-8 mx-auto my-[6px] text-[#071ef3]' />
+                                    </div>
                                     <div className="col_r1"></div>
                                     <div className="col_r1"></div>
                                 </div>
                             </div>
                             <div className="yellow_box">
                                 <div className="yellow_box_middle">
-                                    <div className="yellow_logo"></div>
-                                    <div className="yellow_logo"></div>
-                                    <div className="yellow_logo"></div>
-                                    <div className="yellow_logo"></div>
+                                    <div className="yellow_logo shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"></div>
+                                    <div className="yellow_logo shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"></div>
+                                    <div className="yellow_logo shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"></div>
+                                    <div className="yellow_logo shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"></div>
                                 </div>
                             </div>
                         </div>
