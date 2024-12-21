fd0f1b55d076d98baa22461d44854e0670552c94
Sakib Hossain <sakibs.ngn@gmail.com>
2024-12-21 21:43:09 +0600

    second commit

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
     return (
         <section className="my-[50px]">
             <div className="container">
-                <div className="mx-auto h-[760px] w-[760px] border-[5px] border-[#000000]">
+                <div className="mx-auto h-[760px] w-[760px] border-[5px] border-[#f83949]">
                     <div className="h-[750px] w-[750px]">
                         {/*~~~~~ Top Part Start ~~~~~*/}
                         <div className="part">
@@ -81,7 +81,12 @@ const Banner = () => {
                                 </div>
                             </div>
                             <div className="center_part">
-                                <div className="color"></div>
+                                <div className="color">
+                                    <div className="green_g"></div>
+                                    <div className="yellow_y"></div>
+                                    <div className="blue_b"></div>
+                                    <div className="red_r"></div>
+                                </div>
                             </div>
                             <div className="middle_part">
                                 <div className="mid_row">
